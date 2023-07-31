137c68c387c248bc60954b393d0416f826d653fa
nevena-cvijovic <nevena.cvijovic1@gmail.com>
2023-07-31 15:47:50 +0200

    Register page - Reactive forms applied

--- src/app/auth/register/register.page.ts
+++ src/app/auth/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
+   registerForm: FormGroup= new FormGroup({
+     name: new FormControl('Jovan'),
+     surname: new FormControl(null),
+     email: new FormControl(null),
+     password: new FormControl(null)
+   });
   constructor() { }
 
   ngOnInit() {
+    this.registerForm = new FormGroup({
+      //prvi argument u konstruktoru za formcontrol je default vrednost, drugi argument je validator
+      name: new FormControl('Jovan', Validators.required),
+      surname: new FormControl(null, Validators.required),
+      email: new FormControl(null,[Validators.required, Validators.email]),
+      password: new FormControl(null,[Validators.required, Validators.minLength(7)])
+    });
   }
 
+  onRegister(){
+    console.log(this.registerForm)
+  }
 }
