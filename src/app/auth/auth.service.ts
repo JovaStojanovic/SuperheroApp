f4e06d7ad0ced96eb61089fc22b48e093ed7ef14
nevena-cvijovic <nevena.cvijovic1@gmail.com>
2023-08-02 17:07:46 +0200

    User model created

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import { environment } from 'src/environments/environment';
+import {User} from "./user.model";
 
 export interface AuthResponseData {
   kind: string;
@@ -17,17 +18,29 @@ export interface UserData {
   password: string;
 }
 
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private _isUserAuthenticated = false;
+  private user: User;
+
   constructor(private http: HttpClient) { }
 
   get isUserAuthenticated(){
-    return this._isUserAuthenticated;
+    if(this.user){
+      return !!this.user.token
+    }
+    return false;
   }
 
+  getUserId(){
+    if(this.user){
+  return this.user.id;}
+    return null;
+  }
   register(user: UserData) {
     this._isUserAuthenticated = true;
     return this.http.post<AuthResponseData>(
@@ -47,5 +60,10 @@ export class AuthService {
 
   logout(){
     this._isUserAuthenticated = false;
+    this.user = null;
+
+  }
+  getToken(){
+    return this.user.token;
   }
 }
