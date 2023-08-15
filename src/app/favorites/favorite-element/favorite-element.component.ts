ac479b8f56dd0b446fecd6e09b5afc165fca505a
jovan <jovan.stojanovic36@gmail.com>
2023-08-16 00:43:10 +0200

    Favorites element added

---
+++ src/app/favorites/favorite-element/favorite-element.component.ts
@@ -0,0 +1,29 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Superhero} from "../../superheroes/superhero-element/superhero-model";
+import {Favorite} from "./favorite-model";
+
+@Component({
+  selector: 'app-favorite-element',
+  templateUrl: './favorite-element.component.html',
+  styleUrls: ['./favorite-element.component.scss'],
+})
+export class FavoriteElementComponent  implements OnInit {
+  @Input() superhero: Superhero = {
+    id: "",
+    name: "",
+    description: "",
+    strength: 10,
+    universe: " ",
+    imageUrl: "",
+    user_id: ""
+  }
+  @Input() favorite: Favorite = {
+    id: "",
+    superheroID: "",
+    user_id: ""
+  }
+  constructor() { }
+
+  ngOnInit() {}
+
+}
